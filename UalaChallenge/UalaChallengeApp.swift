7df6ffbd6be5aea74a2e22429d521c3275e923bb
Leandro Berli <leandroberli@gmail.com>
2024-10-16 16:57:33 +0200

    Initial Commit

---
+++ UalaChallenge/UalaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeApp.swift
+//  UalaChallenge
+//
+//  Created by Leandro Berli on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UalaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
