393ebd8fcf450ea58546f5ff1c1c3c66114e5a05
Leandro Berli <leandroberli@gmail.com>
2024-10-21 20:28:03 +0200

    IMPROVEMENT: Change city id prop name

--- UalaChallenge/Models/CityModel.swift
+++ UalaChallenge/Models/CityModel.swift
@@ -7,23 +7,25 @@
 
 import Foundation
 
-public struct City: Codable {
+public struct City: Codable, Identifiable {
     let country: String
     let name: String
-    let _id: Int
+    public let id: Int
     let coord: Coordinates
     var favorite: Bool? = false
+    
+    enum CodingKeys: String, CodingKey {
+            case country
+            case name
+            case id = "_id" // Mapea la propiedad _id a "id" en el JSON
+            case coord  // Mapea coord a "coordinates" en el JSON
+            case favorite
+        }
 }
 
 extension City: Hashable {
     public static func == (lhs: City, rhs: City) -> Bool {
-        lhs._id == rhs._id
-    }
-}
-
-extension City: Identifiable {
-    public var id: Int {
-        return _id
+        lhs.id == rhs.id
     }
 }
 
